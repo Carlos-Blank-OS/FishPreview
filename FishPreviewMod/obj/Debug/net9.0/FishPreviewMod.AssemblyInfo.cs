e1215568750408d78c7de2f3d917cdd2efe7cd8a
carlos-blank <74987790+Carlos-Blank-OS@users.noreply.github.com>
2026-09-03 13:24:55 +0000

    correcao no manifest

--- FishPreviewMod/obj/Debug/net9.0/FishPreviewMod.AssemblyInfo.cs
+++ FishPreviewMod/obj/Debug/net9.0/FishPreviewMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FishPreviewMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a79e5cbb978a1746990f85cc638dfdae39efa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7209056314039578b0239b00144d4e4fb76995")]
 [assembly: System.Reflection.AssemblyProductAttribute("FishPreviewMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FishPreviewMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
